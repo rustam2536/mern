057cfc48be4a99bfe16e9cb6f0b8a048807e5b04
Rustam Ansari <rustam.ansari@agamitechnologies.in>
2022-09-25 11:08:45 +0530

    mongodb connection

--- config/config.js
+++ config/config.js
@@ -2,5 +2,7 @@ const mongoose = require('mongoose');
 main().then(console.log("connected!")).catch(err => console.log(err));
 async function main() {
   // await mongoose.connect('mongodb://localhost:27017/websultanate');
-  await mongoose.connect('mongodb+srv://rustam2536:Rust@123@cluster0.eljaq.mongodb.net/?retryWrites=true&w=majority')
+  const uri = `mongodb://rustam2536:Rust@123@cluster0.eljaq.mongodb.net/websultanate?retryWrites=true&w=majority`;
+  // await mongoose.connect('mongodb+srv://rustam2536:Rust@123@cluster0.eljaq.mongodb.net/?retryWrites=true&w=majority')
+  await mongoose.connect(uri)
 }
